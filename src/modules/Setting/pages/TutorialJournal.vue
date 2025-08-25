93108534311888de265e69ce1ffe15e60b3644e2
iman <inaseri.20@gmail.com>
2025-08-25 14:50:20 +0330

    fix: debug of layout in TutorialJournal.vue

--- src/modules/Setting/pages/TutorialJournal.vue
+++ src/modules/Setting/pages/TutorialJournal.vue
@@ -1,24 +1,26 @@
 <template>
-  <div class="my-5">
-    <form @submit.prevent="submit" :loading="loading">
-      <div class="row my-3">
-        <el-card>
-          <template #header>
-            <span>آموزش ژرنال</span>
-          </template>
-          <div class="row" v-if="!loading">
-            <div class="col-md-12">
-              <EditorField placeholder="توضیحات" label="توضیحات" name="description" required :upload-directory="'tutorial-setting'"/>
+  <div class="container">
+    <div class="my-5">
+      <form @submit.prevent="submit" :loading="loading">
+        <div class="row my-3">
+          <el-card>
+            <template #header>
+              <span>آموزش ژرنال</span>
+            </template>
+            <div class="row" v-if="!loading">
+              <div class="col-md-12">
+                <EditorField placeholder="توضیحات" label="توضیحات" name="description" required :upload-directory="'tutorial-setting'"/>
+              </div>
             </div>
-          </div>
-        </el-card>
-      </div>
-      <div class="row my-3">
-        <div class="col-md-4 mx-auto">
-          <el-button type="success" size="large" class="w-100" native-type="submit" :loading="loadingFile">ثبت </el-button>
+          </el-card>
         </div>
-      </div>
-    </form>
+        <div class="row my-3">
+          <div class="col-md-4 mx-auto">
+            <el-button type="success" size="large" class="w-100" native-type="submit" :loading="loadingFile">ثبت </el-button>
+          </div>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
